29d35ee10ad82f1086203ff9afd9d27a37f673d2
sergeyi7 <poleshhuk97@gmail.com>
2022-04-01 22:40:58 +0600

    Остались только синтаксические правки))))))

--- sea.rb
+++ sea.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 class SeaBattle
-
-  COLUMNS = ('a'..'z').to_a[0..9].freeze
+  COLUMNS = ('a'..'j').to_a.freeze
   ROWS = (0..9).to_a.freeze
-  attr_accessor :win, :list_ships, :wrecked_ships, :free_cell
+  STATUSES = {
+    empty: :empty,
+    busy: :busy,
+    destroyed: :destroyed,
+    missed: :missed
+  }.freeze
+  ASSIGNED_SHIPS = {
+    single_ships: { length_ships: 1, number_ships: 4 },
+    double_ships: { length_ships: 2, number_ships: 3 },
+    triple_ships: { length_ships: 3, number_ships: 2 },
+    quarter_ships: { length_ships: 4, number_ships: 1 }
+  }.freeze
 
-  def initialize()
-    @matrix = []
+  attr_reader :list_ships, :free_cell
+
+  def initialize
+    @field = []
     @list_ships = []
-    @wrecked_ships = []
     @free_cell = []
     generate_free_cell
     building_field
@@ -17,222 +28,231 @@ class SeaBattle
   end
 
   def building_field
-    @x_field = COLUMNS.map.each_with_index {|letter, index| [letter, index]}.to_h
-    ROWS.length.times.each {@matrix.append(['.'] * COLUMNS.length)} 
+    ROWS.each do |row|
+      (0...COLUMNS.length).each do |column|
+        @field << { column: column, row: row, status: STATUSES[:empty] }
+      end
+    end
   end
 
   def view_all_ships
-    clone_matrix = Marshal.load(Marshal.dump(@matrix))
-    @list_ships.flatten(1).each do |x_oxis, y_oxis|
-      new_string = clone_matrix[y_oxis]
-      new_string[x_oxis] = 'S' if new_string[x_oxis] == '.'
-      clone_matrix[y_oxis] = new_string
+    clone_field = Marshal.load(Marshal.dump(@field))
+    clone_field.each do |cell|
+      @list_ships.each do |cell_ships|
+        cell[:status] = STATUSES[:busy] if cell_ships[:row] == cell[:row] && cell_ships[:column] == cell[:column]
+      end
     end
-
-    clone_matrix
+    clone_field
   end
 
-  def fire(let, num)
-    @list_ships.flatten(1).each do |coordinate_cell|
-      if coordinate_cell == [@x_field[let], num]
-        @matrix[num][@x_field[let]] = 'X'
-        @wrecked_ships.append(coordinate_cell)
+  def fire(column, row)
+    column = COLUMNS.index(column)
+    @list_ships.each do |ship_cell|
+      next unless ship_cell.fetch(:column) == column && ship_cell.fetch(:row) == row
 
-        break
-      end
+      @field.find { |cell| cell[:column] == column && cell[:row] == row }[:status] = STATUSES[:destroyed]
+      @list_ships.delete(ship_cell)
 
-      @matrix[num][@x_field[let]] = 'O'
-    end  
-
+      return
+    end
+    @field.find { |cell| (cell[:column] == column) && (cell[:row] == row) }[:status] = STATUSES[:missed]
   end
 
   def generate_random_ships
-    assigned_ships = {single_ships: {length_ship: 1, number_ships: 4}, double_ships: {length_ship: 2, number_ships: 3},
-    triple_ships: {length_ship: 3, number_ships: 2}, quarter_ships: {length_ship: 4, number_ships: 1}}
-    # number_ships = assigned_ships.each_value.map {|value| value[:number_ships]}.sum
-    length_ships = assigned_ships.each_value.map {|value| [value[:length_ship]] * value[:number_ships]}.flatten
-    length_ships.reverse.each do |length|
-      random_direction = rand(0..1)
-      free_zone = free_direction_zone(length)
-      if free_zone[random_direction].empty?
-        random_direction = random_direction == 0 ? 1 : 0
-      end
-      
-      rand_x, rand_y = free_zone[random_direction].sample
-      if [rand_x, rand_y] == [nil, nil]
-        p "Нет больше клеток в этом направлениии!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        next 
-      end
+    ASSIGNED_SHIPS.reverse_each do |_, value|
+      length = value[:length_ships]
 
-      @list_ships.append([])
-      length.times.each do
-        if @list_ships[-1].empty?
-          coordinate = [rand_x, rand_y]
-        else
-          coordinate = @list_ships[-1][-1].clone
-          coordinate[random_direction] +=  1
+      value[:number_ships].times do
+        random_direction = %i[column row].sample
+        free_zone = free_direction_zone(length)
+
+        if free_zone[random_direction].empty?
+          random_direction = random_direction == :row ? :column : :row
         end
 
-        @list_ships[-1].append(coordinate)
-        delete_not_free_cell(coordinate)
-      end
+        rand_cell = free_zone[random_direction].sample
+        rand_column = rand_cell[:column]
+        rand_row = rand_cell[:row]
+        ships_buffer = []
 
+        length.times do
+          if ships_buffer.empty?
+            coordinate = { column: rand_column, row: rand_row }
+          else
+            coordinate = ships_buffer.last.clone
+            coordinate[random_direction] += 1
+          end
+
+          ships_buffer << coordinate
+          @list_ships << coordinate
+
+          delete_not_free_cell(coordinate[:column], coordinate[:row])
+        end
+      end
     end
-    
   end
 
   def command
     puts "\nEnter the coordinates of the ship:"
-    inp = gets.chomp
-    if ["surrender", "s"].any? {|letter| inp.downcase == letter}
-      puts "What a shame…"
-      draw(view_all_ships)
+
+    input = gets.chomp
+
+    if %w[surrender s].any? { |command_view_all| input.downcase == command_view_all }
+      puts 'What a shame…'
+
+      draw(field = view_all_ships)
+
+      exit
     else
-      let, num = inp.scan(/[a-z]/).join(''), inp.scan(/[0-9]/).join('')
-      p num
-      if not num.to_i.between?(1, COLUMNS.length)
-        raise "Ошибка координаты >#{inp}<"
-      elsif not COLUMNS.include?(let.downcase)
-        raise "Ошибка координаты >#{let}<"
+      column = input[/.*[A-z]/]&.downcase
+      row = input[/[0-9].*/] 
+      scan_fail = !row.to_i.between?(1, COLUMNS.length) ||
+                  !COLUMNS.include?(column) || column.length != 1
+      if scan_fail
+        puts 'Ошибка координаты!'
       else
-        num = num.to_i - 1
+        row = row.to_i - 1
+        fire(column, row)
+        draw
       end
-
-      fire(let, num)
-      draw
     end
-
   end
 
   def start
-    @clone_list_ships = Marshal.load(Marshal.dump(@list_ships))
     draw
-    while true
-      begin
-        command
-      rescue => err
-        p err
-      end
 
-      if @list_ships.flatten(1).sort == @wrecked_ships.sort
-        puts "You win!"
+    loop do
+      command
+
+      if @list_ships.empty?
+        puts 'You win!'
+
         break
       end
-
     end
-
   end
 
-  def draw(mx=@matrix)
-    size_last_i = mx.length.to_s.length+2
-    puts ' ' * size_last_i + COLUMNS.join(' ')
-    mx.each_with_index do |rows, index|
-      index += 1
-      puts (index).to_s+' '*(size_last_i - index.to_s.length)+rows.join(' ')
+  def draw(field = @field)
+    puts ' ' * 4 + COLUMNS.join(' ')
+    array_row = []
+
+    ROWS.each do |row|
+      array_row << (row == 9 ? "#{row + 1}  " : "#{row + 1}   ")
+
+      (0..9).to_a.each do |column|
+        cell = field.find { |cell| cell[:row] == row && cell[:column] == column }
+
+        case cell[:status]
+        when STATUSES[:empty]
+          array_row << '. '
+        when STATUSES[:destroyed]
+          array_row << 'X '
+        when STATUSES[:missed]
+          array_row << 'O '
+        when STATUSES[:busy]
+          array_row << 'S '
+        end
+        # if cell[:status] == STATUSES[:empty]
+        #   array_row << '. '
+        # elsif cell[:status] == STATUSES[:destroyed]
+        #   array_row << 'X '
+        # elsif cell[:status] == STATUSES[:missed]
+        #   array_row << 'O '
+        # elsif cell[:status] == STATUSES[:busy]
+        #   array_row << 'S '
+        # end
+      end
+
+      array_row << "\n"
     end
 
+    puts array_row.join('')
   end
 
   private
 
   def generate_free_cell
-    COLUMNS.length.times.each do|num1|
-        ROWS.length.times.each {|num2| @free_cell.append([num1, num2])}
+    ROWS.each do |row|
+      (0...COLUMNS.length).to_a.each do |column|
+        @free_cell << { row: row, column: column }
+      end
     end
-
   end
 
-  def contact_zone(xy)
-    x, y = xy
-    contact = [[x, y], [x-1, y], [x+1, y], [x, y-1], [x, y+1], [x-1, y+1], [x-1, y-1], [x+1, y+1], [x+1, y-1]]
-  end
+  def contact_zone(column, row)
+    states = [0, 1, -1]
+    result = []
 
-
-  def delete_not_free_cell(xy)
-    contact_zone(xy).each do |arr|
-      @free_cell.delete_at(@free_cell.index(arr)) if @free_cell.index(arr) != nil
+    states.each do |state_row|
+      states.each do |state_column|
+        result << { column: column + state_column, row: row + state_row }
+      end
     end
-
+    
+    result
+    # [{column: column, row: row}, {column: column-1, row: row}, {column: column+1, row: row}, {column: column, row: row-1}, {column: column, row: row+1},
+    # {column: column-1, row: row+1}, {column: column-1, row: row-1}, {column: column+1, row: row+1}, {column: column+1, row: row-1}]
   end
-  
+
+  def delete_not_free_cell(column, row)
+    contact_zone(column, row).each do |cell|
+      @free_cell.delete(cell)
+    end
+  end
+
   def free_direction_zone(length)
     directions = {}
-    2.times.each do |direction|
+    %i[column row].each do |direction|
       directions[direction] = []
       @free_cell.each do |cell|
+        row = cell[:row]
+        column = cell[:column]
         next_cells = []
-        length.times.each do      
+        length.times do
           if next_cells.empty?
-            next_cells.append(cell)
+            next_cells << cell
           else
-            next_cell = next_cells[-1].clone
+            next_cell = next_cells.last.clone
             next_cell[direction] += 1
-            next_cells.append(next_cell)
+            next_cells << next_cell
           end
-          
-          x_in_range = next_cells[-1][0].between?(0, COLUMNS.length-1)
-          y_in_range = next_cells[-1][1].between?(0, ROWS.length-1)
-          contact = contact_zone(next_cells[-1]).any? {|element| @list_ships.flatten(1).include?(element)}
-          list_ships_include_coordinate = @list_ships.flatten(1).include?(next_cells[-1])
-          if contact || !x_in_range || !y_in_range
+          column_in_range = next_cells.last[:column].between?(0, COLUMNS.length - 1)
+          row_in_range = next_cells.last[:row].between?(0, ROWS.length - 1)
+          contact = contact_zone(next_cells.last[:column], next_cells.last[:row]).any? do |cell|
+            @list_ships.include?(cell)
+          end
+          if !column_in_range || !row_in_range || contact
             cell = nil
             break
-
           end
-
         end
-
-        if cell != nil
-          directions[direction].append(cell)
-        end
-
+        directions[direction] << cell unless cell.nil?
       end
-
     end
-
     directions
   end
-
 end
-  
+
 sea = SeaBattle.new
 sea.start
 
 def tests(cls)
   a = []
   alphabet = ('a'..'z').to_a[0..9]
-  cls.view_all_ships.each_with_index.each do |arr, i_view_all_ships|
-    arr_m = arr
-    if arr_m.include?('S')
-      arr_m.each_with_index.each do |value, i_in_field|
-        if value == "S"
-          a.append([alphabet[i_in_field], i_view_all_ships+1])
-        end
-
-      end
-
-    end
-
+  cls.view_all_ships.each_with_index.each do |hsh, _index_view_all_ships|
+    a.append([alphabet[hsh.fetch(:column)], hsh.fetch(:row) + 1]) if hsh.fetch(:status) == :busy
   end
-
   a
 end
 
-count = 100
-count.times.each_with_index do |index|
+count = 10_000
+count.times.each do |index|
   test_sea = SeaBattle.new
   a = tests(test_sea)
-  # p "list_ships <==> #{test_sea.list_ships} <==>"
-  # p "free_cell <==> #{test_sea.free_cell} <==>"
-  # test_sea.draw(test_sea.view_all_ships)
-  a.each {|let, num| test_sea.fire(let, num-1)}
-  if !(test_sea.list_ships.flatten(1)-test_sea.wrecked_ships).empty?
-    p "Ошибка"
+  a.each { |column, row| test_sea.fire(column, row - 1) }
+  unless test_sea.list_ships.empty?
+    p 'Ошибка'
     break
   end
-
-  if index == count-1
-    puts "Ошибок Нет!!!)))"
-  end
-
+  puts 'Ошибок Нет!!!)))' if index == count - 1
 end
